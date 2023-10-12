b770fa49dfd8c325b17d1435bc089a0c646cd9f9
Dan Brodjieski <brodjieski@gmail.com>
2023-10-12 15:01:14 -0400

    sync

--- scripts/generate_guidance.py
+++ scripts/generate_guidance.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # filename: generate_guidance.py
 # description: Process a given baseline, and output guidance files
+# made a change to a script
 import sys
 import os.path
 import plistlib
