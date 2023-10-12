5e0ae9ce511c7693f8a1e86e46051f56865c81c8
Dan Brodjieski <brodjieski@gmail.com>
2023-10-12 15:29:01 -0400

    try again

--- scripts/generate_guidance.py
+++ scripts/generate_guidance.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # filename: generate_guidance.py
 # description: Process a given baseline, and output guidance files
-# made a bunch of changes on ventura
+# this should be on every branch
 import sys
 import os.path
 import plistlib
