f575c0c83364eae508e2715fb39f2e9896ae6b9b
Dan Brodjieski <brodjieski@gmail.com>
2023-10-12 15:25:27 -0400

    does this work

--- scripts/generate_guidance.py
+++ scripts/generate_guidance.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # filename: generate_guidance.py
 # description: Process a given baseline, and output guidance files
-# made a change to a script a
+# made a bunch of changes on ventura
 import sys
 import os.path
 import plistlib
